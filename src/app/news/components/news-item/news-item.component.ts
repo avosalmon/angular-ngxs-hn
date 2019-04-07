749dcf5f95245efb3effce5710b852a1381e8e09
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2019-04-07 11:28:11 +0900

    use OnPush ChangeDetectionStrategy

--- src/app/news/components/news-item/news-item.component.ts
+++ src/app/news/components/news-item/news-item.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { News } from '../../models/news.model';
 
 @Component({
   selector: 'app-news-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './news-item.component.html',
   styleUrls: ['./news-item.component.scss']
 })
